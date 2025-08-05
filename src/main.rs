a41dcd07cfa4952fd4a7accdde3c8ec626a77dec
Minhyuk Kim <kmin1318.dev@gmail.com>
2025-08-05 20:21:31 +0900

    feat: remove create_example_mapping

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use blog_os::memory;
-    use x86_64::{structures::paging::Page, VirtAddr};
+    use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
     blog_os::init();
@@ -21,14 +21,6 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator =
         unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf0000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe {
-        page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e);
-    };
 
     #[cfg(test)]
     test_main();
