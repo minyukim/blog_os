7885bf9163d1c549a2a9ff85b1251073e1eacd73
Minhyuk Kim <kmin1318.dev@gmail.com>
2025-08-06 20:13:13 +0900

    feat: use heap in kernel_main

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
-use blog_os::println;
-use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+
+use bootloader::{entry_point, BootInfo};
+
+use blog_os::println;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use blog_os::memory;
+    use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
+
     use x86_64::VirtAddr;
+
+    use blog_os::{allocator, memory};
 
     println!("Hello World{}", "!");
     blog_os::init();
@@ -25,7 +29,28 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator =
         unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    let x = Box::new(41);
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
